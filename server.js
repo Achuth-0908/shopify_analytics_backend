80944a876b55aba8158c3025b4bd4d05a5ee47ae
G Achuth <achuthganesh09@gmail.com>
2025-09-15 20:03:57 +0530

    Deploying..

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(helmet());
 
 // CORS configuration for Next.js frontend
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL,
-    /\.vercel\.app$/,
-  ].filter(Boolean),
+  origin: process.env.FRONTEND_URL?.replace(/\/$/, ""),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-tenant-id']
